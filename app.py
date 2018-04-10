b3b0927b9dbb42fddaef2bc77c247f0eae49badb
Andrew Yuan <yuandrew@umich.edu>
2018-04-10 16:48:14 -0400

    what we did in lecture

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import model
 
 app = Flask(__name__)
@@ -13,9 +13,25 @@ def index():
         </ul>
     '''
 
-@app.route('/bball')
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
+
+@app.route('/bball', methods=['GET', 'POST'])
 def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
+
+    return render_template("seasons.html", seasons=seasons)
+
 
 if __name__ == '__main__':
+    model.init_bball()
     app.run(debug=True)
